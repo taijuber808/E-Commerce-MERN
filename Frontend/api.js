a19790bb1c050e47678b28d84bd6f01cea3b9f6c
Tai JUber <taijuber808@gmail.com>
2026-09-24 01:03:08 +0530

    configure backend for render

--- Frontend/api.js
+++ Frontend/api.js
@@ -1,5 +1,4 @@
-const BASE_URL = "http://localhost:8080/api";
-
+const BASE_URL = "https://e-commerce-mern-ovo3.onrender.com/api";
 // ==================== CATEGORY ====================
 
 export const getCategories = async () => {
